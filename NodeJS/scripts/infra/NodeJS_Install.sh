d417fbd631a64273c84827d343e2c82289c96cf4
hood-boi <salman.adams@mail.utoronto.ca>
2020-06-19 21:15:03 -0400

    NodeJS Install Script

---
+++ NodeJS/scripts/infra/NodeJS_Install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rf .node/
+wget https://nodejs.org/dist/v14.4.0/node-v14.4.0-linux-x64.tar.xz
+tar -xf node-v14.4.0-linux-x64.tar.xz
+rm node-v14.4.0-linux-x64.tar.xz
+mv node-v14.4.0-linux-x64 .node
+cd .node/
+echo "export NODE_HOME=`pwd`" >> ~/.bashrc
+export NODE_HOME=`pwd`
+echo "alias node='$NODE_HOME/bin/node'" >> ~/.bashrc
+echo "alias npm='$NODE_HOME/bin/npm'" >> ~/.bashrc
